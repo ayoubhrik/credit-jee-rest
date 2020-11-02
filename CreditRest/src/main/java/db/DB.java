6ed5ed5caf45e9b41fc1bbb3d840923c67bc939f
ayoubhrik <73712615+ayoubhrik@users.noreply.github.com>
2020-11-02 12:43:19 +0100

    Update DB.java

--- CreditRest/src/main/java/db/DB.java
+++ CreditRest/src/main/java/db/DB.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
-	/*
+	
 	private static Connection c;
 	static {
 		try {
@@ -21,23 +21,7 @@ public class DB {
 	}
 	public static Connection getConnection() {
 		return c;
-	}*/
+	}
 	
-	private static Connection c;
-	static {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");	
-			c = DriverManager.getConnection("jdbc:mysql://w1kr9ijlozl9l79i.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/tjjk3gm62ggkhnso", "n2jkd5lzsqduol89" , "bs1tzbxt3i6z9axj");
-		} catch (ClassNotFoundException e) {
-			System.err.println("DB problem!");
-			e.printStackTrace();
-		} catch (SQLException e) {
-			System.err.println("SQL statement error!");
-			e.printStackTrace();
-		}
-	}
-	public static Connection getConnection() {
-		return c;
-	}
 
 }
